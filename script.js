8b8d001459472ab35f560883fdebb08e8092fc61
The Silent App Coder <diretoria.g2d@gmail.com>
2023-07-10 13:43:57 -0300

    Alteração de avatares e perfil do instagram dinamico

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
+  let userElement = document.getElementById("users")
 
   if (html.classList.contains("light")) {
+    console.log(userElement)
     img.setAttribute("src", "./assets/avatar-light.png")
+    userElement.textContent = "@perrarolf"
   } else {
     img.setAttribute("src", "./assets/avatar.png")
+    userElement.textContent = "@graziugione"
   }
 }
